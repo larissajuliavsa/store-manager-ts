00dfca0ac6b766ab0c851cb395d6cd9b5b1f0e3d
Larissa Araujo <larissa.jvsa@gmail.com>
2023-12-17 22:10:42 -0300

    chore: add express config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const text = "hello world!"
-console.log(text)
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world")
+})
+
+app.listen(3000, () => {
+  console.log("listening on port 3000")
+})
