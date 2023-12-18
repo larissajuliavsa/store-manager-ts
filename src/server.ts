c59fc2457afca74dc1100032097956d00c4ce6b9
Larissa Araujo <larissa.jvsa@gmail.com>
2023-12-17 22:56:39 -0300

    chore: add dockerfile and docker compose config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import * as express from "express"
 import { Request, Response } from "express"
 
 const app = express()
+const PORT = 3000
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world")
 })
 
 app.listen(3000, () => {
-  console.log("listening on port 3000")
+  console.log(`listening on port ${PORT}`)
 })
